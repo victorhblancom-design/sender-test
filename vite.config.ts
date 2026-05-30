b43eaf9ecbe702f1e79eee69186355561dc82d15
victorhblancom-design <victorhblancom@gmail.com>
2026-05-29 21:06:10 -0600

    Update Tailwind CSS integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), tailwindcss(), svgLoader()],
 });
